d472a3f68aefd470f38aa57c984206d882b83b9b
Ashish Gaikwad <gaikwadashish2477@gmail.com>
2026-02-06 15:54:18 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,8 @@ import { componentTagger } from "lovable-tagger";
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
+    base: "rootandroutine",
+    plugins: [react()],
     host: "::",
     port: 8080,
     hmr: {
